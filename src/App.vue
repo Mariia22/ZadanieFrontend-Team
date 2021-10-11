5720bfb785fc4cd3b02e5f2687fb305656940dbd
Maria22 <m.ushakova22@gmail.com>
2021-10-11 14:28:13 +0200

    add component table for workers data

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <table-pracownicy />
+  </div>
 </template>
 
 <script>
-//import HelloWorld from "./components/HelloWorld.vue";
+import TablePracownicy from "@/components/TablePracownicy.vue";
 
 export default {
   name: "App",
-  components: {},
+  components: {
+    TablePracownicy,
+  },
 };
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/TablePracownicy.vue
@@ -0,0 +1,33 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Imie</th>
+        <th>Nazwisko</th>
+        <th>Dzial</th>
+        <th>Wynagrodzenie</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tfoot>
+  </table>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
